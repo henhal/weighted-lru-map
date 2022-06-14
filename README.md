949fa6a351b1b0a1f3eef8616c8588662993c593
Henrik Hall <henrik.hall@sony.com>
2022-06-14 14:38:13 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # weighted-lru-map
-A LRU map supporting eviction based on calculated item weight
+A LRU map supporting eviction based on calculated item weight in addition to the item count.
+
+## Introduction
+
+This is an extension of the [lru_map](https://www.npmjs.com/package/lru_map) NPM module, adding support for cache eviction based on item weight, in addition to the limit on the item count.
+The weight of each item is calculated by a `getWeight` function supplied to the `WeightedLruMap` constructor.
+
+This allows effecient LRU caching of items where the size varies significantly. With a traditional LRU map using only item count
+as the eviction criteria, adding 20 small items or 20 huge items would render the same number of evictions, making it
+hard to predict the RAM usage of a cache allowing 1 000 items of unknown size.
+With a `WeightedLruMap`, eviction can be set up to occur based on approximate memory usage, gaining more control over
+eviction and perhaps thereby allowing larger caches with better hit rate without risking out of memory.
+
+## Installation
+
+```
+$ npm install 'weighted-lru-map';
+```
+
+## Examples:
+
+1. Cache of arrays with length of each array as the weight
+```
+import {WeightedLruMap} from 'weighted-lru-map';
+
+/**
+ * Create a LRU map storing string arrays as values, 
+ * where the item weight is the length of each array
+ */
+const map = new WeightedLruMap<number, string[]>(value => value.length, 10);
+
+map.set(1, ['A', 'B', 'C']); // weight is now 3
+map.set(2, ['D', 'E', 'F']); // weight is now 6
+map.set(3, ['G', 'H']); // weight is now 8
+map.set(4, ['I', 'J', 'K', 'L']); // weight became 12, evicted item 1 to reach weight 8
+map.set(5, ['M']); // weight is 9
+map.set(6, ['N', 'O', 'P', 'Q', 'R']); // weight became 14, evicted items 2 and 3 to 
+                                       // reach weight 9
+```
+
+2. Cache of objects with the JSON representation of each object as the weight
+```
+import {WeightedLruMap} from 'weighted-lru-map';
+
+/**
+ * Create a LRU map storing arbitrary objects, where the item weight is the length of 
+ * the JSON representation of each item
+ */
+const map = new WeightedLruMap<number, any>(value => JSON.stringify(value).length, 50);
+
+map.set(1, {foo: 42}); // weight is now 10
+map.set(2, {foo: 43}); // weight is now 20
+map.set(3, {foo: 44}); // weight is now 30
+map.set(4, {foo: 42, bar: 'awesome!', hello: 'world!'}); // weight became 74, evicting 
+                                                         // items 1, 2 and 3 to reach weight 44
+...
+```
---
+++ src/WeightedLruMap.ts
@@ -0,0 +1,95 @@
+import {LRUMap} from 'lru_map';
+
+/**
+ * A LRU map with eviction based on the combined item weight in addition to the number of items (=size).
+ * The weight of an item is calculated using the getWeight function supplied to the constructor.
+ *
+ * If no limit is specified, it's set to unlimited, meaning items will be evicted only when the weight surpasses
+ * the weight limit, never on the item count.
+ * If limit AND weight limit are both specified, eviction will take place whenever size > limit OR
+ * weight > weightLimit.
+ * Note that multiple items may be evicted on a single insert, as inserting a single heavy item may push out several
+ * lighter items.
+ */
+export default class WeightedLruMap<K, V> extends LRUMap<K, V> {
+  private _weight = 0;
+
+  /**
+   *
+   * @param getWeight A function calculating the weight of an item
+   * @param weightLimit The weight limit. When the combined weight surpasses this limit, items are evicted.
+   * @param [limit] Limit on the number of items. If absent, no limit is applied.
+   * @param [entries] Initial entries
+   */
+  constructor(
+      private readonly getWeight: (entry: V, key: K) => number,
+      readonly weightLimit: number,
+      limit = Number.MAX_VALUE,
+      entries?: Iterable<[K,V]>
+  ) {
+    super(limit, entries);
+
+    if (weightLimit <= 0) {
+      throw new Error('Weight limit must be a positive number');
+    }
+  }
+
+  /**
+   * The combined weight of the items in the map
+   */
+  get weight() {
+    return this._weight;
+  }
+
+  assign(entries: Iterable<[K, V]>) {
+    super.assign(entries);
+
+    this._weight = 0;
+    for (const [key, value] of entries) {
+      this._weight += this.getWeight(value, key);
+    }
+  }
+
+  clear() {
+    super.clear();
+
+    this._weight = 0;
+  }
+
+  delete(key: K): V | undefined {
+    const value = super.delete(key);
+
+    if (value !== undefined) {
+      this._weight -= this.getWeight(value, key);
+    }
+
+    return value;
+  }
+
+  set(key: K, value: V): this {
+    const itemSize = this.getWeight(value, key);
+    const oldValue = this.get(key);
+    const oldItemSize = oldValue !== undefined ? this.getWeight(oldValue, key) : 0;
+
+    super.set(key, value);
+
+    this._weight += itemSize - oldItemSize;
+
+    while (this._weight > this.weightLimit) {
+      this.shift();
+    }
+
+    return this;
+  }
+
+  shift(): [K, V] | undefined {
+    const entry = super.shift();
+
+    if (entry) {
+      const [key, value] = entry;
+      this._weight -= this.getWeight(value, key);
+    }
+
+    return entry;
+  }
+}
